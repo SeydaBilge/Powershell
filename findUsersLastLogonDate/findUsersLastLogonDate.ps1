2a79118ba6a4d653e3245dd77115ac1c8ddeacee
SeydaBilge <seydabilgeunver@outlook.com>
2022-07-31 00:13:09 +0300

    lastLogonDate3Month

---
+++ findUsersLastLogonDate/findUsersLastLogonDate.ps1
@@ -0,0 +1,12 @@
+﻿
+$date = (Get-Date).AddMonths(-3)
+$TargetOU = "OU=SilinecekKullanicilar,DC=sbu,DC=local"
+
+#$user = Get-ADUser -Filter * -SearchBase "OU=Users,DC=sbu,DC=local" -Properties "LastLogonDate","SamAccountName" | where {$.LastLogonDate -lt $date} Sort-Object -property LastLogonDate,SamAccountName,DistinguishedName -Descending | FT SamAccountName, @{name="LastLogonDate";expression={($.LastLogonDate).ToShortDAteString()}}, DistinguishedName -AutoSize
+
+$user = Get-ADUser -Filter * -SearchBase "OU=Users,DC=sbu,DC=local" -Properties "LastLogonDate","SamAccountName" | where {$.LastLogonDate -lt $date -and $.Enabled -like "True"} select SamAccountName
+
+foreach ($user in $users)
+{
+    Get-ADUser $user.SamAccountName | Move-ADObject -TargetPath $TargetOU -Confirm
+}
